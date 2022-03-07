533968f068cf977b4b3c75845e5320b1f747986c
Ugo DANIEL-THOMAS <ugdaniel@42.student.fr>
2022-03-07 11:33:05 +0100

    update

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -6,33 +6,43 @@
 /*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 10:30:39 by ugdaniel          #+#    #+#             */
-/*   Updated: 2022/03/07 11:01:12 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2022/03/07 11:32:29 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
-static void	replace_lines_in_file(std::ifstream *f, std::ofstream *r, const char **av)
+static void	replace_lines_in_file(std::ifstream *f, const char **av)
 {
 	std::string		line;
+	std::ofstream	replace_file;
+	std::string		replace_path;
 
-	while (std::getline(*f, line))
+	replace_path = std::string(av[1]) + std::string(".replace"); // Add '.replace' to our file name
+	replace_file.open(replace_path, std::ios::out);
+	if (!replace_file.is_open())
+	{
+		std::cerr << "error: could not create replacement file" << std::endl;
+		return ;
+	}
+	while (std::getline(*f, line) && (*f).is_open())
 	{
 		if (line.compare(std::string(av[2])) == 0)
-			*r << std::string(av[3]);
+			replace_file << std::string(av[3]);
 		else
-			*r << line;
-		if ((*f).peek() != EOF)
-        	*r << std::endl;
+			replace_file << line;
+		if ((*f).peek() == EOF) // Stop if file doesn't end with a newline
+			break ;
+		replace_file << std::endl;
 	}
+	replace_file.close();
+	std::cout << av[1] << " > " << replace_path << std::endl;
 }
 
 int	main(int argc, const char **argv)
 {
 	std::ifstream	file;
-	std::ofstream	replace;
-	std::string		replace_path;
 
 	if (argc != 4)
 	{
@@ -42,20 +52,10 @@ int	main(int argc, const char **argv)
 	file.open(argv[1], std::ios::in);
 	if (!file.is_open())
 	{
-		std::cerr << "error: could not open file" << std::endl;
+		std::cerr << "error: could not open file: " << argv[1] << std::endl;
 		return (1);
 	}
-	replace_path = std::string(argv[1]) + std::string(".replace");
-	replace.open(replace_path, std::ios::out);
-	if (!replace.is_open())
-	{
-		file.close();
-		std::cerr << "error: could not create replace file" << std::endl;
-		return (1);
-	}
-	replace_lines_in_file(&file, &replace, argv);
+	replace_lines_in_file(&file, argv);
 	file.close();
-	replace.close();
-	std::cout << argv[1] << " > " << replace_path << std::endl;
 	return (0);
 }
