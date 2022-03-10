5152ec28b11cc9e360fc79a030fc4f7f83e047a2
Ugo DANIEL-THOMAS <ugdaniel@42.student.fr>
2022-03-10 14:54:45 +0100

    updated tests

--- 03/ex00/main.cpp
+++ 03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 22:11:36 by ugdaniel          #+#    #+#             */
-/*   Updated: 2022/03/10 12:18:28 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2022/03/10 14:54:24 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,8 +46,9 @@ int	main(void)
 	claptrap2.attack("Blue target");
 	claptrap2.attack("Green target");
 	claptrap2.attack("Red target");
-	claptrap2.attack("Blue target");	// Can't attack, no energy left!
-	claptrap2.attack("Green target");
-	claptrap2.beRepaired(20);			// Can't be repaired, no energy left!
+	claptrap2.attack("Blue target");	// No energy left from this point
+	claptrap2.takeDamage(10);
+	claptrap2.beRepaired(20);
+	claptrap2.takeDamage(10);
 	return (0);
 }
