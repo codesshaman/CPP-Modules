da5823b92e324de2ddae17617c42e0d57931521f
Ugo DANIEL-THOMAS <ugdaniel@42.student.fr>
2022-03-10 13:04:07 +0100

    scavtrap

---
+++ 03/ex01/ScavTrap.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/10 12:14:22 by ugdaniel          #+#    #+#             */
+/*   Updated: 2022/03/10 12:25:15 by ugdaniel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#if !defined SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+	public:
+		ScavTrap(void);
+		ScavTrap(std::string);
+		ScavTrap	& operator=(const ScavTrap &);
+		~ScavTrap(void);
+		void	guardGate(void);
+};
+
+#endif /* !defined SCAVTRAP_HPP */
---
+++ 03/ex01/main.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/09 22:11:36 by ugdaniel          #+#    #+#             */
+/*   Updated: 2022/03/10 13:03:29 by ugdaniel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
+int	main(void)
+{
+	ClapTrap	claptrap1("CL4P-TP1");
+	ScavTrap	scavtrap1("SC4V-TP1");
+	ScavTrap	scavtrap2("SC4V-TP2");
+
+	scavtrap1.attack("Red target");
+	scavtrap1.attack("Red target");
+	scavtrap1.attack("Red target");
+	scavtrap1.guardGate();
+	scavtrap1.takeDamage(5);
+	scavtrap1.attack("Red target");
+	scavtrap1.beRepaired(5);
+	scavtrap1.attack("Red target");
+	scavtrap1.takeDamage(20);
+	scavtrap1.attack("Red target");
+	scavtrap1.takeDamage(200);
+	scavtrap1.beRepaired(20);
+	
+	scavtrap2.guardGate();
+	scavtrap2.attack("Green target");
+	scavtrap2.takeDamage(30);
+	scavtrap2.beRepaired(300);
+	scavtrap2.attack("Blue target");
+	scavtrap2.takeDamage(1000);
+	scavtrap2.attack("Red target");
+	return (0);
+}
