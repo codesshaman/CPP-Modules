8dbca39d67388d9661a398f2889b5fe499f837be
Ugo DANIEL-THOMAS <ugdaniel@42.student.fr>
2022-03-11 13:10:35 +0100

    updated tests

--- 04/ex00/main.cpp
+++ 04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 16:50:54 by ugdaniel          #+#    #+#             */
-/*   Updated: 2022/03/10 19:41:11 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2022/03/11 13:10:21 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 
 int	main(void)
 {
-	{
+	{ // subject tests
 		const Animal* meta = new Animal();
 		const Animal *j = new Dog();
 		const Animal *i = new Cat();
@@ -35,7 +35,7 @@ int	main(void)
 		delete (i);
 	}
 	std::cout << "-------------" << std::endl;
-	{
+	{ // WrongAnimal tests
 		const WrongAnimal *w = new WrongAnimal();
 		const WrongCat *c = new WrongCat();
 
@@ -47,17 +47,24 @@ int	main(void)
 		delete (c);
 	}
 	std::cout << "-------------" << std::endl;
-	{
+	{ // more tests
 		Animal	animal;
 		Cat		cat;
 		Dog		dog;
+		Animal	*cat2 = new Cat();
+		Animal	*dog2 = new Dog();
 
-		animal.getType();
+		std::cout << "\'animal\' type is: " << animal.getType() << std::endl;
 		animal.makeSound();
-		cat.getType();
+		std::cout << "\'cat\' type is: " << cat.getType() << std::endl;
 		cat.makeSound();
-		dog.getType();
+		std::cout << "\'dog\' type is: " << dog.getType() << std::endl;
 		dog.makeSound();
+
+		std::cout << "\'cat2\' type is: " << cat2->getType() << std::endl;	// cat2 and dog2 are instances of Animal
+		cat2->makeSound();													// but compiler still calls the expected
+		std::cout << "\'dog2\' type is: " << dog2->getType() << std::endl;	// Cat and Dog functions
+		dog2->makeSound();
 	}
 	return (0);
 }
