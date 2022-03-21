4d0062f4b0219faa2be1afef641fa5a7c3504c31
Ugo DANIEL-THOMAS <ugdaniel@42.student.fr>
2022-03-21 17:02:12 +0100

    updated tests

--- 07/ex01/main.cpp
+++ 07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 22:05:32 by ugdaniel          #+#    #+#             */
-/*   Updated: 2022/03/21 11:49:30 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2022/03/21 17:01:50 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,28 +20,19 @@ template <typename T> void	print(const T &a)
 	std::cout << a;
 }
 
-// test purposes
-static void	print(const int &a)
-{
-	std::cout << a;
-}
-
 int	main(void)
 {
 	int			intarr[5] = {0, 1, 2, 3, 4};
 	char		chararr[3] = {'a', 'b', 'c'};
 	std::string	strarr[4] = {"Hel", "lo, w", "orl", "d!"};
 
-	// print(const int &)
-	iter(intarr, 5, print);	 
+	iter(intarr, 5, print<int>);	 
 	std::cout << std::endl;
 
-	// print(const T &)
-	iter(chararr, 3, print);
+	iter(chararr, 3, print<char>);
 	std::cout << std::endl;
 
-	// print(const T &)
-	iter(strarr, 4, print);
+	iter(strarr, 4, print<std::string>);
 	std::cout << std::endl;
 	return (0);
 }
