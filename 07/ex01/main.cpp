a61f1ef11bc20ddd584cae2f5e6582e55d286ad8
Ugo DANIEL-THOMAS <ugdaniel@42.student.fr>
2022-03-21 11:49:44 +0100

    updated tests

--- 07/ex01/main.cpp
+++ 07/ex01/main.cpp
@@ -6,31 +6,42 @@
 /*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 22:05:32 by ugdaniel          #+#    #+#             */
-/*   Updated: 2022/03/20 22:16:47 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2022/03/21 11:49:30 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 #include <iostream>
+#include <string>
 
-template <typename T> void	print(const T &a) // test purposes
+// test purposes
+template <typename T> void	print(const T &a) 
 {
-	std::cout << a << " ";
+	std::cout << a;
 }
 
-static void	print(const int &a)	// test purposes
+// test purposes
+static void	print(const int &a)
 {
-	std::cout << a << " ";
+	std::cout << a;
 }
 
 int	main(void)
 {
-	int		intarr[5] = {0, 1, 2, 3, 4};
-	char	chararr[3] = {'a', 'b', 'c'};
+	int			intarr[5] = {0, 1, 2, 3, 4};
+	char		chararr[3] = {'a', 'b', 'c'};
+	std::string	strarr[4] = {"Hel", "lo, w", "orl", "d!"};
 
-	iter(intarr, 5, print);	 // this will call the int version of print
+	// print(const int &)
+	iter(intarr, 5, print);	 
 	std::cout << std::endl;
-	iter(chararr, 3, print); // this will call the template version of print
+
+	// print(const T &)
+	iter(chararr, 3, print);
+	std::cout << std::endl;
+
+	// print(const T &)
+	iter(strarr, 4, print);
 	std::cout << std::endl;
 	return (0);
 }
