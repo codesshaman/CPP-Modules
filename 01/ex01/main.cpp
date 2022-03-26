d1957d0de266ef7e3dfa009d7bc94cd55b04b46d
Ugo DANIEL-THOMAS <ugdaniel@42.student.fr>
2022-03-26 17:18:09 +0100

    added error handling

--- 01/ex01/main.cpp
+++ 01/ex01/main.cpp
@@ -3,37 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ugdaniel <ugdaniel@42.student.fr>          +#+  +:+       +#+        */
+/*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/06 12:18:42 by ugdaniel          #+#    #+#             */
-/*   Updated: 2022/03/06 13:56:34 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2022/03/26 17:17:21 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 #include <string>
+#include <iostream>
 
 Zombie  *zombieHorde(int N, std::string name);
 
-static void	zombies(int N, std::string name)
+static void	create_zombie_horde(int N, std::string name)
 {
 	int		i;
 	Zombie	*horde;
 
     horde = zombieHorde(N, name);
+	if (!horde)
+		return ;
 	for (i = 0; i < N; i++)
 		horde[i].announce();
-	
-	// Horde is an array: add brackets before pointer
+
 	delete [] horde;
 }
 
 int main(void)
 {
-	zombies(1, "1 zombie");
-	zombies(5, "5 zombies");
-	zombies(10, "10 zombies");
-	zombies(0, "0 zombies");
-	zombies(2, "2 zombies");
+	create_zombie_horde(1, "1 zombie");
+	create_zombie_horde(5, "5 zombies");
+	create_zombie_horde(10, "10 zombies");
+	create_zombie_horde(0, "0 zombies");
+	create_zombie_horde(2, "2 zombies");
     return (0);
 }
--- 01/ex01/zombieHorde.cpp
+++ 01/ex01/zombieHorde.cpp
@@ -3,20 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   zombieHorde.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ugdaniel <ugdaniel@42.student.fr>          +#+  +:+       +#+        */
+/*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/06 12:17:55 by ugdaniel          #+#    #+#             */
-/*   Updated: 2022/03/06 13:15:38 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2022/03/26 17:16:42 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+#include <iostream>
 
 Zombie  *zombieHorde(int N, std::string name)
 {
     int     i;
 	Zombie	*zombie_horde = new Zombie[N];
 
+	if (!zombie_horde)
+	{
+		std::cerr << "[DEBUG]: error: could not allocate a new horde of " << N << " zombie(s)" << std::endl;
+		return (nullptr);
+	}
     for (i = 0;i < N; i++)
         zombie_horde[i].setZombieName(name);
 	return (zombie_horde);
