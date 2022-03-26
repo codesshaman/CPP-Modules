88b6ca16b05bb57c6cc58ceb5e580902d8d17d17
Ugo DANIEL-THOMAS <ugdaniel@42.student.fr>
2022-03-26 17:21:27 +0100

    fixes: one call to cout is enough

--- 01/ex02/main.cpp
+++ 01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/06 14:34:31 by ugdaniel          #+#    #+#             */
-/*   Updated: 2022/03/06 17:05:05 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2022/03/26 17:20:42 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 
 int main(void)
 {
-	std::string	str = "HI THIS IS STRING";
+	std::string	str = "HI THIS IS BRAIN";
 	std::string	*stringPTR = &str;
 	std::string	&stringREF = str;
 
-	std::cout << "Address of string:\t\t" << &str << std::endl;
-	std::cout << "Address held by stringPTR:\t" << stringPTR << std::endl;
-	std::cout << "Address held by stringREF:\t" << &stringREF << std::endl;
-	std::cout << std::endl;
-	std::cout << "Value of string:\t\t" << str << std::endl;
-	std::cout << "Value pointed to by stringPTR:\t" << *stringPTR << std::endl;
-	std::cout << "Value pointed to by stringREF:\t" << stringREF << std::endl;
+	std::cout << "Address of string:\t\t" << &str << std::endl
+		<< "Address held by stringPTR:\t" << stringPTR << std::endl
+		<< "Address held by stringREF:\t" << &stringREF << std::endl
+		<< std::endl
+		<< "Value of string:\t\t" << str << std::endl
+		<< "Value pointed to by stringPTR:\t" << *stringPTR << std::endl
+		<< "Value pointed to by stringREF:\t" << stringREF << std::endl;
 
 	return (0);
 }
