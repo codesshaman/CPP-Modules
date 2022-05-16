0b9ad8b8cd2688bec6c3573e96b48c3cdf5671e3
Ugo DANIEL-THOMAS <ugdaniel@42.student.fr>
2022-05-16 21:50:15 +0200

    08

--- 08/ex01/Span.cpp
+++ 08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 14:37:20 by ugdaniel          #+#    #+#             */
-/*   Updated: 2022/04/05 16:04:32 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2022/05/16 21:44:50 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,23 @@
 #include <stdexcept>
 #include <cstdlib>
 
-Span::Span(void): _max_size(0)
+Span::Span()
+	: _max_size(0)
 {
-	return ;
 }
 
-Span::Span(unsigned int max_size): _max_size(max_size)
+Span::Span(unsigned int max_size)
+	: _max_size(max_size)
 {
-	return ;
 }
 
-Span::Span(const Span &src): _max_size(src._max_size)
+Span::Span(const Span &src)
+	: _max_size(src._max_size)
 {
-	return ;
 }
 
-Span	&Span::operator=(const Span &src)
+Span&
+Span::operator=(const Span& src)
 {
 	if (this == &src)
 		return (*this);
@@ -44,32 +45,29 @@ Span	&Span::operator=(const Span &src)
 
 Span::~Span(void)
 {
-	return ;
 }
 
-void	Span::addNumber(int nb)
+void
+Span::addNumber(int nb)
 {
 	if (this->_numbers.size() >= this->_max_size)
 		throw (std::length_error("size limit reached"));
 	this->_numbers.push_back(nb);
 }
 
-int	Span::shortestSpan(void) const
+int
+Span::shortestSpan(void) const
 {
-	int							ret;
-	int							tmp;
-	std::vector<int>::iterator	it;
-	std::vector<int>::iterator	it2;
-	std::vector<int>			numbers = this->_numbers;
+	std::vector<int> numbers = this->_numbers;
 
 	if (numbers.size() <= 1)
 		throw std::logic_error("not enough numbers to return a span");
-	ret = -1;
-	for (it = numbers.begin(); it + 1 != numbers.end(); it++)
+	int ret = -1;
+	for (std::vector<int>::iterator it = numbers.begin(); it + 1 != numbers.end(); it++)
 	{
-		for (it2 = it + 1; it2 != numbers.end(); it2++)
+		for (std::vector<int>::iterator it2 = it + 1; it2 != numbers.end(); it2++)
 		{
-			tmp = *it2 > *it ? *it2 - *it : *it - *it2;
+			int tmp = *it2 > *it ? *it2 - *it : *it - *it2;
 			if (ret == -1 || tmp < ret)
 				ret = tmp;
 		}
@@ -77,22 +75,19 @@ int	Span::shortestSpan(void) const
 	return (ret);
 }
 
-int	Span::longestSpan(void) const
+int
+Span::longestSpan(void) const
 {
-	int							ret;
-	int							tmp;
-	std::vector<int>::iterator	it;
-	std::vector<int>::iterator	it2;
-	std::vector<int>			numbers = this->_numbers;
+	std::vector<int> numbers = this->_numbers;
 
 	if (numbers.size() <= 1)
 		throw std::logic_error("not enough numbers to return a span");
-	ret = -1;
-	for (it = numbers.begin(); it + 1 != numbers.end(); it++)
+	int ret = -1;
+	for (std::vector<int>::iterator it = numbers.begin(); it + 1 != numbers.end(); it++)
 	{
-		for (it2 = it + 1; it2 != numbers.end(); it2++)
+		for (std::vector<int>::iterator it2 = it + 1; it2 != numbers.end(); it2++)
 		{
-			tmp = *it2 > *it ? *it2 - *it : *it - *it2;
+			int tmp = *it2 > *it ? *it2 - *it : *it - *it2;
 			if (ret == -1 || tmp > ret)
 				ret = tmp;
 		}
@@ -100,26 +95,28 @@ int	Span::longestSpan(void) const
 	return (ret);
 }
 
-void	Span::fillRandomNumbers(void)
+void
+Span::fillRandomNumbers(void)
 {
-	int		nb_left;
-
-	nb_left = this->_max_size - this->getSize();
+	int nb_left = this->_max_size - this->getSize();
 	for (int i = 0; i < nb_left; i++)
 		this->addNumber(rand() % 42 + 1);
 }
 
-unsigned int	Span::getMaxSize(void) const
+unsigned int
+Span::getMaxSize(void) const
 {
-	return (this->_max_size);
+	return _max_size;
 }
 
-int	Span::getSize(void) const
+int
+Span::getSize(void) const
 {
-	return (this->_numbers.size());
+	return _numbers.size();
 }
 
-std::ostream	& operator<<(std::ostream &o, const Span &s)
+std::ostream&
+operator<<(std::ostream& o, const Span& s)
 {
 	o << "numbers limit: " << s.getMaxSize() << ", size: " << s.getSize() << ", shortest span: " << s.shortestSpan() << ", longest span: " << s.longestSpan();
 	return (o);
--- 08/ex02/MutantStack.hpp
+++ 08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 18:12:36 by ugdaniel          #+#    #+#             */
-/*   Updated: 2022/04/05 15:59:59 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2022/05/16 21:49:01 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,28 @@
 # include <stack>
 #include <deque>
 
-template <typename T>
-class MutantStack : public std::stack<T>
+template <class Tp>
+class MutantStack : public std::stack<Tp>
 {
-	public:
-		typedef typename std::stack<T>::container_type::iterator		iterator;
-		typedef typename std::stack<T>::container_type::const_iterator	const_iterator;
+public:
+	typedef typename std::stack<Tp>::container_type::iterator       iterator;
+	typedef typename std::stack<Tp>::container_type::const_iterator const_iterator;
 
-		/* construct/copy/destroy*/
-		MutantStack(void) {}
-		~MutantStack(void) {}
-		MutantStack(const MutantStack<T> &src) {*this = src;}
+	MutantStack(void) {}
+	~MutantStack(void) {}
+	MutantStack(const MutantStack& src) {*this = src;}
 
-		MutantStack<T>	&	operator=(const MutantStack<T> &rhs) {
-			std::stack<T>::operator=(rhs);
-			return (*this);
-		}
+	MutantStack& operator=(const MutantStack& x)
+	{
+		std::stack<Tp>::operator=(x);
+		return (*this);
+	}
 
-		/* iterators */
-		iterator			begin(void)			{return (this->c.begin());}
-		const_iterator		begin(void)	const	{return (this->c.begin());}
-		iterator			end(void)			{return (this->c.end());}
-		const_iterator		end(void)	const	{return (this->c.end());}
+	iterator       begin(void)       {return (this->c.begin());}
+	const_iterator begin(void) const {return (this->c.begin());}
 
+	iterator       end(void)         {return (this->c.end());}
+	const_iterator end(void)   const {return (this->c.end());}
 };
 
 #endif /* MUTANT_STACK_HPP */
